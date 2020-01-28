9c417a55e310d3ceacccba5e0b6f23ecea0a8115
Estevanus <oscartoreh@gmail.com>
2020-01-29 05:09:13 +0800

    Add files via upload

---
+++ OscDesktop/DesktopForm.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace OscDesktop
+{
+	partial class DesktopForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.menuStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
+			this.explorerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.volumeSettingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.wallpaperChanger = new System.Windows.Forms.Timer(this.components);
+			this.menuStrip.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// menuStrip
+			// 
+			this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.explorerToolStripMenuItem,
+            this.volumeSettingsToolStripMenuItem});
+			this.menuStrip.Name = "menuStrip";
+			this.menuStrip.Size = new System.Drawing.Size(160, 48);
+			// 
+			// explorerToolStripMenuItem
+			// 
+			this.explorerToolStripMenuItem.Name = "explorerToolStripMenuItem";
+			this.explorerToolStripMenuItem.Size = new System.Drawing.Size(159, 22);
+			this.explorerToolStripMenuItem.Text = "&Explorer";
+			this.explorerToolStripMenuItem.Click += new System.EventHandler(this.ExplorerToolStripMenuItem_Click);
+			// 
+			// volumeSettingsToolStripMenuItem
+			// 
+			this.volumeSettingsToolStripMenuItem.Name = "volumeSettingsToolStripMenuItem";
+			this.volumeSettingsToolStripMenuItem.Size = new System.Drawing.Size(159, 22);
+			this.volumeSettingsToolStripMenuItem.Text = "&Volume Settings";
+			this.volumeSettingsToolStripMenuItem.Click += new System.EventHandler(this.VolumeSettingsToolStripMenuItem_Click);
+			// 
+			// wallpaperChanger
+			// 
+			this.wallpaperChanger.Enabled = true;
+			this.wallpaperChanger.Interval = 3000;
+			this.wallpaperChanger.Tick += new System.EventHandler(this.WallpaperChanger_Tick);
+			// 
+			// DesktopForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackgroundImage = global::OscDesktop.Properties.Resources._281446_aliya06;
+			this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+			this.ClientSize = new System.Drawing.Size(1366, 768);
+			this.ContextMenuStrip = this.menuStrip;
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+			this.Name = "DesktopForm";
+			this.ShowIcon = false;
+			this.ShowInTaskbar = false;
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "OscDesktop";
+			this.Load += new System.EventHandler(this.DesktopForm_Load);
+			this.menuStrip.ResumeLayout(false);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+		private System.Windows.Forms.ContextMenuStrip menuStrip;
+		private System.Windows.Forms.ToolStripMenuItem explorerToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem volumeSettingsToolStripMenuItem;
+		private System.Windows.Forms.Timer wallpaperChanger;
+	}
+}
+
---
+++ OscDesktop/DesktopForm.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OscDesktop
+{
+	public partial class DesktopForm : Form
+	{
+		string relativePath = AppDomain.CurrentDomain.BaseDirectory;
+		string myDocPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+		string userPath = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+		bool winPressed = false;
+
+		List<Bitmap> daftarWalpapers = new List<Bitmap>();
+		List<Image> dw = new List<Image>();
+		int curWallpaper = 0;
+
+		private System.Windows.Forms.ListView desktopCollection;
+
+		ImageList iList = new ImageList();
+
+		public DesktopForm()
+		{
+			InitializeComponent();
+			KeyUp += DesktopForm_KeyUp;
+			KeyDown += DesktopForm_KeyDown;
+
+			desktopCollection = new CustomControl();
+
+			desktopCollection.HideSelection = false;
+			desktopCollection.Location = new System.Drawing.Point(543, 92);
+			desktopCollection.Name = "desktopCollection";
+			desktopCollection.Size = new System.Drawing.Size(335, 218);
+			desktopCollection.TabIndex = 1;
+			desktopCollection.UseCompatibleStateImageBehavior = false;
+			desktopCollection.Dock = DockStyle.Fill;
+
+			Controls.Add(desktopCollection);
+
+		}
+
+		private void DesktopForm_KeyDown(object sender, KeyEventArgs e)
+		{
+			if (e.KeyCode == Keys.LWin)
+			{
+				winPressed = true;
+			}
+		}
+
+		void GetDesktopFiles()
+		{
+			System.IO.DirectoryInfo pardir = new System.IO.DirectoryInfo(userPath);
+
+			System.IO.DirectoryInfo[] dirs = pardir.GetDirectories();
+			System.IO.FileInfo[] files = pardir.GetFiles();
+
+			foreach (System.IO.DirectoryInfo dir in dirs)
+			{
+				ListViewItem item = new ListViewItem(dir.Name);
+				item.ImageKey = "folder";
+				desktopCollection.Items.Add(item);
+			}
+
+			foreach (System.IO.FileInfo file in files)
+			{
+				ListViewItem item = new ListViewItem(file.Name);
+				item.ImageKey = "file";
+				desktopCollection.Items.Add(item);
+			}
+		}
+
+		private void DesktopForm_Load(object sender, EventArgs e)
+		{
+
+			//Bitmap gb = new Bitmap(@"wallpapers/wallpaper.jpg");
+			iList.ImageSize = new System.Drawing.Size(42, 42);
+			Icon folderIcon = new Icon(relativePath + "/icons/folder.ico");
+			Icon fileIcon = new Icon(relativePath + "icons/file.ico");
+			iList.Images.Add("file", fileIcon);
+			iList.Images.Add("folder", folderIcon);
+			desktopCollection.LargeImageList = iList;
+			desktopCollection.SmallImageList = iList;
+			
+
+			System.IO.StreamReader f = System.IO.File.OpenText(relativePath + "/wallpaperList.txt");
+			string isi = f.ReadToEnd();
+			f.Close();
+
+			string[] baris = isi.Split('\n');
+
+			//MessageBox.Show(relativePath);
+
+			foreach (string lok in baris)
+			{
+				Size s = new Size(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+				Bitmap wallpaper = new Bitmap(@lok);
+				Bitmap tes = new Bitmap(wallpaper, s);
+				//wallpaper.SetResolution(600, 400);
+				daftarWalpapers.Add(tes);
+			}
+
+			//BackgroundImage = gb;
+			//desktopCollection.BackgroundImage = gb;
+
+			//desktopCollection.BackgroundImage = this.BackgroundImage;
+
+			GetDesktopFiles();
+
+
+			ChangeBackGround();
+		}
+
+		private void DesktopForm_KeyUp(object sender, KeyEventArgs e)
+		{
+			if (winPressed == true && e.KeyCode == Keys.E)
+			{
+				System.Diagnostics.Process.Start("OscExplorer.exe");
+				Console.WriteLine("okey dokey");
+			}
+
+			if (e.KeyCode == Keys.LWin)
+			{
+				winPressed = false;
+			}
+		}
+
+		private void ExplorerToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			System.Diagnostics.Process.Start("OscExplorer.exe");
+		}
+
+		private void VolumeSettingsToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			System.Diagnostics.Process.Start("C://Windows/System32/SndVol.exe");
+		}
+
+		void ChangeBackGround()
+		{
+			if (curWallpaper < daftarWalpapers.Count)
+			{
+				desktopCollection.BackgroundImage = daftarWalpapers[curWallpaper];
+				curWallpaper += 1;
+			}
+			else
+			{
+				curWallpaper = 0;
+			}
+		}
+
+		private void WallpaperChanger_Tick(object sender, EventArgs e)
+		{
+			ChangeBackGround();
+		}
+	}
+}
